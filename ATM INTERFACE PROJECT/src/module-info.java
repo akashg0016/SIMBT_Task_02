0bc227ead1b2a84245b67b067e85988de8cb237d
Akash Gupta <109038021+akashg0016@users.noreply.github.com>
2023-03-24 19:39:12 +0530

    Add files via upload

---
+++ ATM INTERFACE PROJECT/src/module-info.java
@@ -0,0 +1,2 @@
+module TASK_ATM {
+}
---
+++ ATM INTERFACE PROJECT/src/project/atm/AtmOperationInterf.java
@@ -0,0 +1,8 @@
+package project.atm;
+
+public interface AtmOperationInterf {
+    public void viewBalance();
+    public void withdrawAmount(double withdrawAmount);
+    public void depositAmount(double depositAmount );
+    public  void viewTranscationHistory();
+}
